b9ed0d8f32933d1138b9d163bd05022b14c5635f
Marucik <rakoczy.mk@gmail.com>
2017-01-01 14:44:46 +0100

    Refaktor koloryzowania
    Pozdro @Dawiducik

--- js/colors.js
+++ js/colors.js
@@ -1,27 +1,16 @@
 var navElements = document.getElementsByTagName('LI');
-navElements[0].addEventListener("click",function clrChange() {
-  clrR = 150;
-  clrG = 0;
-  clrB = 0;
-  for (var i = 0; i < navElements.length; i++) {
-    navElements[i].style.backgroundColor = "rgba(200, 0, 0, 0.9)";
-  }
-})
 
-navElements[1].addEventListener("click",function clrChange() {
-  clrR = 0;
-  clrG = 150;
-  clrB = 0;
+function clrChange(r = 0, g = 0, b = 0){
+  return function(){
+  clrR = r;
+  clrG = g;
+  clrB = b;
   for (var i = 0; i < navElements.length; i++) {
-    navElements[i].style.backgroundColor = "rgba(0, 200, 0, 0.9)";
+    navElements[i].style.backgroundColor = "rgba(" + r + "," + g + "," + b + ", 0.9)";
+    }
   }
-})
+}
 
-navElements[2].addEventListener("click",function clrChange() {
-  clrR = 0;
-  clrG = 0;
-  clrB = 150;
-  for (var i = 0; i < navElements.length; i++) {
-    navElements[i].style.backgroundColor = "rgba(0, 0, 200, 0.9)";
-  }
-})
+navElements[0].addEventListener("click", clrChange(150, 0, 0));
+navElements[1].addEventListener("click", clrChange(0, 150, 0));
+navElements[2].addEventListener("click", clrChange(0, 0, 150));
